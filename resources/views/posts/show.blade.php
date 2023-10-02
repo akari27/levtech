c47dcfeb13278b1c1e2e367160f49d6490c6ab3f
akari27 <mhn12ka@gmail.com>
2023-10-02 23:33:27 +0900

    8-5 課題5

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,10 +8,12 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
         <h1 class="title">
             {{ $post->title }}
         </h1>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">編集</a>
+        </div>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
